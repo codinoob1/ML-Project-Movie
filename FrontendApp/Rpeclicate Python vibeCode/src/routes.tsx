b2c34da747a2f1cfeefcb46bde5c78a9de83c802
Sahilq <duddely1233@gmail.com>
2026-09-02 21:26:31 +0400

    Createing the frontend Desgine

--- FrontendApp/Rpeclicate Python vibeCode/src/routes.tsx
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import Landing from "./Landing";
-import MovieRoom from "./MovieRoom";
-
-export const router = createBrowserRouter([
-  { path: "/", Component: Landing },
-  { path: "/app", Component: MovieRoom },
-]);
